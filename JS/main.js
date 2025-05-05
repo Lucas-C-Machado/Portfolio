07a4eb5f6ac1d64cb550aed8113cd7681faf70a8
Dev-Lucas_C_Machado <lucascamponogaramachado@gmail.com>
2025-05-05 11:07:26 -0300

    Remove todos os arquivos do repositório remoto

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-/*Main*/
-
-/*Arquivo principal que pode ser usado para inicialização global*/
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Portfólio carregado com sucesso!")
-  })
-  
